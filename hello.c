4b8e7575c69812df51b55c881abc974e67a2a703
Malllikarjun77 <karraallikarjun0077@gmail.com>
2025-06-16 16:21:54 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World! Welcome to India from our beloved people..........\n");
+    printf("Hello, World! Welcome to India\n");
     return 0;
 }
